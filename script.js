a2d336f54eabb98ce1462841577d2118bc707a77
Gabrielloop <gabriel.henin@gmail.com>
2024-11-29 21:51:48 +0100

    Ended Efficiency & Lap Time

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// A FAIRE
+// Ajouter une fonction dans un objets : utilisation de this !
+// transformer en objet les tableaux et utiliser for in pour les parcourir.
+
 // PARAMETRES DE COURSE
 // PNEUS
-const tyreDurationS = 20;
-const tyreDurationM = 30;
-const tyreDurationH = 40;
 
 const tyres = [
     {
@@ -23,6 +24,8 @@ const tyres = [
 ];
 // TOURS
 const totalLap = 70;
+const averageTimeLapInMille = 90000;
+const tyreImpactMaxInMille = 10000;
 // UTILISATEURS
 let currentLap = 0;
 let currentTyres = {
@@ -41,18 +44,39 @@ const tyresStrategy = [
         pitLap: 30,
     },
     {
-        tyreType: "S",
-        pitLap: 50,
+        tyreType: "M",
+        pitLap: 40,
     },
     {
         tyreType: "H",
-        pitLap: 60,
+        pitLap: 80,
     },
 ];
 
 // TABLEAU D'AFFICHAGE DES ARRETS
 // const pitStrategy = tyresStrategy.map(Element => Element.pitLap);
 // console.log(pitStrategy);
+
+
+function formatTime(milliseconds) {
+    // Récupération des minutes et millisecondes restantes
+    const minutes = Math.floor(milliseconds / 60000);
+    const remainMillis = milliseconds % 60000;
+  
+    // Récupération des secondes
+    const seconds = Math.floor(remainMillis / 1000);
+    const millis = remainMillis % 1000;
+  
+
+    const formattedMinutes = String(minutes).padStart(2, "0");
+    const formattedSeconds = String(seconds).padStart(2, "0");
+    let formattedMillis=millis.toFixed(3);
+    formattedMillis = String(formattedMillis).padStart(3, "0");
+  
+    // Construire la chaîne de temps
+    return `${formattedMinutes}:${formattedSeconds}.${formattedMillis}`;
+  }
+console.log("Temps moyen : " + formatTime(averageTimeLapInMille));
 
 // Fonction de controle de la présence d'un changement de stratégie, donc arret aux stands
 // return : ("tyretype")
@@ -80,13 +104,16 @@ function tyresAge(pitLapTyresChanged, currentLapProp) {
     return ageTyres;
 }
 
-//Fonction de calcul de l'éfficacité
+//Fonction de calcul de l'efficacité
 // 1/exp(x)
 function efficiency(tyresAgeEff, tyresTypeEFF) {
-    efficiencyI=1/Math.exp(tyresAgeEff/20);
-    efficiencyI=Number(efficiencyI).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2});
+    efficiencyI = 1 / Math.exp(tyresAgeEff / 20);
     return efficiencyI;
 }
+function formatpourcent(decimal){
+    return Number(decimal).toLocaleString(undefined, { style: 'percent', minimumFractionDigits: 2 });
+}
+
 
 for (i = 0; i < totalLap + 1; i++) {
     currentLap++;
@@ -97,6 +124,7 @@ for (i = 0; i < totalLap + 1; i++) {
     // Préparation du message à afficher dans la console
     const msgLap = "Tour n°" + currentLap;
     const msgCurrentTyres = currentTyres.tyreType + " tyres (age :" + tyresAge(currentTyres.pitLap, i) + " lap)";
-    const msgEfficiency = efficiency(tyresAge(currentTyres.pitLap, i), "M")
-    console.log(msgLap + "-" + msgCurrentTyres + " " + msgEfficiency);
+    const msgEfficiency = formatpourcent(efficiency(tyresAge(currentTyres.pitLap, i), "M"));
+    const msgLapTime = formatTime(averageTimeLapInMille - efficiency(tyresAge(currentTyres.pitLap, i), "M")*tyreImpactMaxInMille);
+    console.log(msgLap + "-" + msgCurrentTyres + " " + msgEfficiency + " " + msgLapTime);
 }
