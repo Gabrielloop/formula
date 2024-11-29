ade168c848cafb8ad4599636511ee9f98c12a448
Gabrielloop <gabriel.henin@gmail.com>
2024-11-29 14:44:57 +0100

    Star adding efficiency

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 // PARAMETRES DE COURSE
-
+// PNEUS
 const tyreDurationS = 20;
 const tyreDurationM = 30;
 const tyreDurationH = 40;
+// TOURS
+const totalLap = 70;
+// UTILISATEURS
+let currentLap = 0;
+let currentTyres = {
+    tyreType: "??",
+    pitLap: 0
+};
 
 // PARAMETRES DE LA STRATEGIE APPLIQUEE
 const tyresStrategy = [
@@ -24,22 +32,54 @@ const tyresStrategy = [
     },
 ];
 
-const pitStrategy = tyresStrategy.map(Element => Element.pitLap);
-
-console.log(pitStrategy);
+// TABLEAU D'AFFICHAGE DES ARRETS
+// const pitStrategy = tyresStrategy.map(Element => Element.pitLap);
+// console.log(pitStrategy);
 
 // Fonction de controle de la présence d'un changement de stratégie, donc arret aux stands
-// return : TRUE / FALSE
+// return : ("tyretype")
 function pitLapControl(lap) {
     const idPitLap = tyresStrategy.findIndex(element => element.pitLap == lap);
-    return idPitLap>=0;
+        if(idPitLap>=0){
+            console.warn("New tyres : " + tyresStrategy[idPitLap].tyreType);
+            return tyresStrategy[idPitLap].tyreType;
+        }
 }
 
+// Fonction de changement des pneusc
+// return : undefine
+function pitChangeTyres(typeOfTyres, acutalLap) {
+    currentTyres = {
+        tyreType: typeOfTyres,
+        pitLap: acutalLap
+    };
+}
 
-const totalLap = 70;
-let currentLap = 0;
+// Fonction de calcul de l'age des pneus
+// return : i
+function tyresAge(pitLapTyresChanged, currentLapProp){
+    const ageTyres=currentLapProp-pitLapTyresChanged;
+    return ageTyres;
+}
+
+//Fonction de calcul de l'éfficacité
+
+function efficiency(tyresAgeEff, ) {
+
+    return ;
+}
 
 for (i = 0; i < totalLap + 1; i++) {
+    currentLap++;
+    // CONTROLE DE LA STRATEGIE
+    if (pitLapControl(i)!=null){
+        pitChangeTyres(pitLapControl(i), i)
+    } else {}
 
-    console.log("Tour n°" + i + " " + pitLapControl(i))
+    const msgLap = "Tour n°" + currentLap;
+    const msgCurrentTyres = currentTyres.tyreType + " tyres (" + tyresAge(currentTyres.pitLap, i) + " lap)";
+
+
+
+    console.log(msgLap + "-" + msgCurrentTyres);
 }
